835273159fa4374f4e31ee6b6c85ebe9b6074af0
José Pedro Silva <64554534+Josepe75@users.noreply.github.com>
2021-10-21 13:18:56 +0200

    Update conf.py

--- src/pvlab/doc/source/conf.py
+++ src/pvlab/doc/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, Silva J.P.'
 author = 'Silva J.P'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0.dev1'
+release = '0.1.0.dev2'
 
 # The major project version, use as the replacement for |version|
 version = '0.1.0'
