91359aa71560fec593c6d1b4281203b249aed9c0
José Pedro Silva <64554534+Josepe75@users.noreply.github.com>
2021-10-22 11:50:09 +0200

    Update __init__.py

--- src/pvlab/dataframes/__init__.py
+++ src/pvlab/dataframes/__init__.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # sub-package dataframes
-"""Provide tools to manage dataframes in the context of calibration.
-
-It contains the following python modules:
-"""
+"""Provide tools to manage dataframes in the context of calibration."""
