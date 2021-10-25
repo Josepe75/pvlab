9ae2124233db86cfbf5fb0058666b5f901b460b9
José Pedro Silva <64554534+Josepe75@users.noreply.github.com>
2021-10-25 07:28:41 +0200

    Update test_module.py

--- src/pvlab/test/test_module.py
+++ src/pvlab/test/test_module.py
@@ -8,19 +8,19 @@ Created on Mon May 31 07:00:52 2021.
 @author: josepedro
 """
 import unittest
-from pvlab.tools.math.hypot import hypot
+from pvlab.tools.math.module import module
 
 
-class HypotTest(unittest.TestCase):
+class ModuleTest(unittest.TestCase):
     def test_result(self):
-        """Check if the result of hypot() function is correct"""
-        self.assertEqual(11.576, round(hypot(5, 8, 3, 6), 3))
+        """Check if the result of module() function is correct"""
+        self.assertEqual(11.576, round(module(5, 8, 3, 6), 3))
 
     def test_exception(self):
-        """Check if hypot generates an exception when\
+        """Check if module generates an exception when\
  combining strings and integer or floats"""
         with self.assertRaises(TypeError):
-            hypot(5, 8, 3, '6')
+            module(5, 8, 3, '6')
 
 
 if __name__ == '__main__':
