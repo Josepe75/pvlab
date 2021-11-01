186acf1f1d7d3a6aa44a96d069fae3f6fd44db40
José Pedro Silva <64554534+Josepe75@users.noreply.github.com>
2021-11-01 01:06:58 +0100

    Update __init__.py
    Extend initial description of ``pvlab`` package.

--- src/pvlab/__init__.py
+++ src/pvlab/__init__.py
@@ -10,11 +10,14 @@ The package ``pvlab``provides tools for solar sensors and pv devices.
 
 PVLAB is a project devoted to the development and improvement of scientific
 software for the measurement, calibration and modeling of the performance of
-photoltaic devices and solar sensors. PVLAB package was born from the efforts
-made to perform the data treatment for the calibration of pyranometers at
-the Laboratory of Photovoltaic Solar Energy (PVLAb), in the Research Center
-for Energy, Environment and Technology (CIEMAT) in Madrid, Spain. The aim of
-PVLAB is to provide a set of tools, mainly consisting in classes and
-functions, to perform the data treatment for the calibration of pyranometers.
+photovoltaic devices and solar sensors. PVLAB package born from the efforts
+in data treatment for the calibration of pyranometers at
+the Laboratory of Photovoltaic Solar Energy (**PVLab**), in the Research Center
+for Energy, Environment and Technology (**CIEMAT**) in Madrid, Spain. The aim of
+``pvlab`` is to provide a set of tools, mainly consisting in classes and
+functions, to facilitate task related to calibration of solar sensors and
+photovoltaic devices, like: data treatment (I/O, filtering, etc.),
+calculation of responsivity, determination of total uncertainty, validation test,
+analysis of results, etc.
 """
 __version__ = '0.1.0.dev7'
